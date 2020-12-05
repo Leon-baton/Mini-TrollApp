b04a2dcff62807019bb5b5bf690b18c931b4e307
Leon-baton <60987723+Leon-baton@users.noreply.github.com>
2020-12-06 01:00:55 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ del /f /s /q build
 set "path=%LocalAppData%\bin\NASM;%path%"
 mkdir build
 nasm -f win ./main.asm -Ox -o ./build/main.obj
-golink /console ./build/main.obj user32.dll kernel32.dll
+golink /console ./build/main.obj user32.dll kernel32.dll /fo ./build/App.exe
 pause
